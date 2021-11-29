97675b1b840d67823c33bf4243c61bb75cfce117
Abhishek Singhal <abhisinghal112001@gamil.com>
2021-11-29 21:25:57 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,16 @@
+let onav=document.getElementById("open-nav");
+let cnav=document.getElementById("close-nav");
+let allbdy=document.getElementById("section");
+let header=document.getElementById("header");
+
+let nav=document.getElementById("nav");
+onav.addEventListener('click',()=>{
+    nav.style.right="0"
+    // header.style.right="30vw"
+    allbdy.style.filter="blur(3px)"
+})
+cnav.addEventListener('click',()=>{
+    nav.style.right="-30vw"
+    // header.style.right="0vw"
+    allbdy.style.filter="none"
+})
