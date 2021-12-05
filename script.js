db35d3d05a0813795ae7a4f688b04dc2c84003ff
Abhishek Singhal <abhisinghal112001@gamil.com>
2021-12-05 14:31:59 +0530

    header

--- script.js
+++ script.js
@@ -3,19 +3,3 @@ var loader = document.querySelector(".preloader");
 window.addEventListener("load",()=>{
     loader.style.display="none";
 })
-let onav=document.getElementById("open-nav");
-let cnav=document.getElementById("close-nav");
-let allbdy=document.getElementById("section");
-let header=document.getElementById("header");
-
-let nav=document.getElementById("nav");
-onav.addEventListener('click',()=>{
-    nav.style.right="0"
-    // header.style.right="30vw"
-    allbdy.style.filter="blur(3px)"
-})
-cnav.addEventListener('click',()=>{
-    nav.style.right="-60vw"
-    // header.style.right="0vw"
-    allbdy.style.filter="none"
-})
