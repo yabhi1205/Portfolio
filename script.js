9347b524bb454e0ad24285771ac0999bb00719e2
Abhishek Singhal <abhisinghal112001@gamil.com>
2021-12-08 18:27:15 +0530

    nav

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var loader = document.querySelector(".preloader");
 window.addEventListener("load",()=>{
     loader.style.display="none";
 })
+//======================header scroll=================
+var Fname = document.querySelector(".h-name");
+let sImage = document.querySelector("#sImage1");
+let work = document.querySelector(".roll");
+let shape1 = document.querySelector(".shape1");
+document.addEventListener('scroll',()=>{
+    let value = scrollY;
+    Fname.style.transform="translateY("+(value)+"px)";
+    sImage.style.transform="translateY("+(-value)+"px)";
+    work.style.transform="translateY("+(value)+"px)";
+    shape1.style.transform="rotateZ("+(value/10)+"deg)";
+})
