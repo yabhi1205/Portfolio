97b7443552108f1be704b8743c1bf87280c91df0
Abhishek Singhal <abhisinghal112001@gamil.com>
2022-01-08 10:43:51 +0530

    contact

--- script.js
+++ script.js
@@ -110,3 +110,120 @@ window.addEventListener("scroll",()=>{
 
    }
 });
+
+//======================contact===================
+let progg= document.querySelector(".Cprogress");
+let next = document.querySelector(".Cnext");
+let prev = document.querySelector(".Cprevious");
+let countProg = 0;
+prev.addEventListener('click',()=>{
+    if(countProg!=0){
+        countProg-=25;
+        next.style.cursor="pointer";
+        next.style.color="white";
+        progg.innerText=countProg+"%";
+        if(countProg==25){
+            progg.style.background="conic-gradient(aqua 25%,white 50%,white 75%,white 100% )";
+        }
+        if(countProg==50){
+            progg.style.background="conic-gradient(aqua 25%,aqua 50%,white 75%,white 100% )";
+        }
+        if(countProg==75){
+            progg.style.background="conic-gradient(aqua 25%,aqua 50%,aqua 75%,white 100% )";
+        }
+        if(countProg==0){
+            prev.style.color="rgba(245, 245, 245, 0.3)";
+            prev.style.cursor="not-allowed";
+            progg.style.background="conic-gradient(white 25%,white 50%,white 75%,white 100% )";
+            
+        }
+    }
+    swap();
+});
+next.addEventListener('click',()=>{
+    if(countProg!=100){
+        countProg+=25;
+        prev.style.cursor="pointer";
+        next.style.cursor="pointer";
+        prev.style.color="white";
+        progg.innerText=countProg+"%";
+        if(countProg==25){
+            progg.style.background="conic-gradient(aqua 25%,white 50%,white 75%,white 100% )";
+        }
+        if(countProg==50){
+            progg.style.background="conic-gradient(aqua 25%,aqua 50%,white 75%,white 100% )";
+        }
+        if(countProg==75){
+            progg.style.background="conic-gradient(aqua 25%,aqua 50%,aqua 75%,white 100% )";
+        }
+        if(countProg==100){
+            next.style.color="rgba(245, 245, 245, 0.3)";
+            next.style.cursor="not-allowed";
+            progg.style.background="conic-gradient(aqua 25%,aqua 50%,aqua 75%,aqua 100% )";
+            
+        }
+    }
+    swap();
+});
+
+function swap(){
+    let conform = document.querySelectorAll(".formBox");
+    conform.forEach((e)=>{
+        e.style.left="-200vw"
+    });
+    if(countProg==0){
+        conform[0].style.right="0";
+        conform[0].style.left="0";
+    }
+    if(countProg==25){
+        conform[1].style.right="0";
+        conform[1].style.left="0";
+    }
+    if(countProg==50){
+        conform[2].style.right="0";
+        conform[2].style.left="0";
+    }
+    if(countProg==75){
+        conform[3].style.right="0";
+        conform[3].style.left="0";
+    }
+    if(countProg==100){
+        conform[4].style.right="0";
+        conform[4].style.left="0";
+    }
+}
+
+//========================mail============================
+document.getElementById("submit").addEventListener("click",()=>{
+    let Fname = document.getElementById("Fname").value;
+    let Lname = document.getElementById("Lname").value;
+    let Phone = document.getElementById("num").value;
+    let mail = document.getElementById("mail").value;
+    let des = document.getElementById("detail").value;
+    var bodys ="Name: "+ Fname + " "+Lname +"<br/> Phone no.: "+Phone+"<br/> Email Id.:  "+mail+"<br/> Description: "+des;
+    
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "abhisinghal112001@gmail.com",
+        Password : "teigugjsovguukpt",
+        To : 'abhisinghal112001@gmail.com',
+        From : mail,
+        Subject : Fname+" Sent you a mail (Portfolio)",
+        Body : bodys
+    }).then(
+        message =>{
+            if(message=='OK'){
+                progg.innerText="Done";
+                setInterval(()=>{
+                    location.reload();
+                },2000)
+            }
+            else{
+                alert('There is an error in sending message. Or you missed some area')
+            }
+        }
+    );
+})
+
+
+    
