dc1f613614b517c0fde0a0ab49a03d47d90ce348
Abhishek Singhal <abhisinghal112001@gamil.com>
2022-01-09 11:20:25 +0530

    animation .2

--- script.js
+++ script.js
@@ -35,57 +35,11 @@ close.addEventListener("click",()=>{
 
 //======================skills =================
 
-let count1=0;
-let count2=0;
-let count3=0;
-let count4=0;
-let count5=0;
-let count6=0;
-run();
-let html = document.getElementById("html-skill");
-let css = document.getElementById("css-skill");
-let js = document.getElementById("js-skill");
-let cLanguage = document.getElementById("c-skill");
-let java = document.getElementById("java-skill");
-let edit = document.getElementById("edit-skill");
-setInterval(run,11050)
-function run(){
-    count1=0;
-    count2=0;
-    count3=0;
-    count4=0;
-    count5=0;
-    count6=0;
-    let time = setInterval(showword,100);
-}
-function showword() {
-    if(count1<90){
-        count1+=2;
-    }
-    html.innerText=count1+'%';
-    if(count2<65){
-        count2++;
-    }
-    css.innerText=count2+'%';
-    if(count3<55){
-        count3++;
-    }
-    js.innerText=count3+'%';
-    if(count4<55){
-        count4++;
-    }
-    cLanguage.innerText=count4+'%';
-    if(count5<40){
-        count5++;
-    }
-    java.innerText=count5+'%';
-    if(count6<50){
-        count6++;
-    }
-    edit.innerText=count6+'%';
-}
 
+//==============scroll=====================
 //=================sections=================
+let progg= document.querySelector(".Cprogress");
+let link= document.querySelectorAll(".link");
 let bottomPage=window.innerHeight/6*2;
 let section = document.querySelectorAll("section");
 window.addEventListener("scroll",()=>{
@@ -97,24 +51,20 @@ window.addEventListener("scroll",()=>{
                 e.classList="center nactive";
             }
         });
+        for(let i=0;i<6;i++){
+            let len=section[i].getBoundingClientRect().top;
+            if(len>=-10 && len<=bottomPage){
+                 link.forEach((e)=>{
+                     e.classList="link";
+                 });
+                 link[i].classList="link active"
+            }
+        }
 });
     
-//==============scroll=====================
-let link = document.querySelectorAll(".link");
-window.addEventListener("scroll",()=>{
-   for(let i=0;i<6;i++){
-       let len=section[i].getBoundingClientRect().top;
-       if(len>=-10 && len<=bottomPage){
-            link.forEach((e)=>{
-                e.classList="link";
-            });
-            link[i].classList="link active"
-       }
-   }
-});
+
 
 //======================contact===================
-let progg= document.querySelector(".Cprogress");
 let next = document.querySelector(".Cnext");
 let prev = document.querySelector(".Cprevious");
 let countProg = 0;
@@ -171,7 +121,7 @@ next.addEventListener('click',()=>{
 function swap(){
     let conform = document.querySelectorAll(".formBox");
     conform.forEach((e)=>{
-        e.style.left="-200vw"
+        e.style.left="-300vw"
     });
     if(countProg==0){
         conform[0].style.right="0";
@@ -226,9 +176,3 @@ document.getElementById("submit").addEventListener("click",()=>{
         }
     );
 })
-
-
-
-
-
-    
